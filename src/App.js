b412f6209fe8de8726c48d121151d31c80031169
Prathamesh Vaishnav <133900444+PabloEscobaar0@users.noreply.github.com>
2024-11-08 17:23:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to My Simple React App!</h1>
+        <h1>Welcome to My Simple React App!!!!</h1>
         <p>This is a single-page React app.</p>
       </header>
     </div>
